9e4c929e8f7e463e8cb281440a5c26300b97ebb0
KT147 <kevin.tammm@gmail.com>
2025-11-06 14:04:57 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,111 @@
+const buttonColours = ["red", "blue", "green", "yellow"];
+const gamePattern = [];
+const userClickedPattern = [];
+
+let level = 0;
+let started = false;
+
+document.addEventListener("keydown", function () {
+  if (!started) {
+    started = true;
+    nextSequence();
+  }
+});
+
+document.querySelectorAll(".btn").forEach(function (button) {
+  button.addEventListener("click", function () {
+    const userChosenColour = button.id;
+    userClickedPattern.push(userChosenColour);
+
+    animatePress(button);
+    checkAnswer(userChosenColour);
+  });
+});
+
+function nextSequence() {
+  level++;
+  document.querySelector("#level-title").textContent = "Level " + level;
+
+  const randomNumber = Math.floor(Math.random() * 3);
+  const randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  playSound(randomChosenColour);
+
+  const element = document.querySelector("#" + randomChosenColour)
+  animatePress(element);
+
+  console.log(gamePattern);
+}
+
+function checkAnswer(currentLevel) {
+
+  if (gamePattern.slice(0, userClickedPattern.length).join() === userClickedPattern.join()) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(() => {
+        nextSequence();
+        console.log("succsss");
+        userClickedPattern.length = 0;
+      }, 1000);
+    }
+  
+
+  } else {
+    playSound("wrong");
+    document.querySelector("body").classList.add("game-over");
+    setTimeout(() => {
+      document.querySelector("body").classList.remove("game-over");
+    }, 200);
+    startOver();
+
+    document.querySelector("#level-title").textContent =
+      "Game Over, Press Any Key to Restart";
+    console.log("wrong");
+  }
+}
+
+function startOver() {
+  gamePattern.length = 0;
+  userClickedPattern.length = 0;
+  level = 0;
+  started = false;
+}
+
+function animatePress(currentColour) {
+  currentColour.classList.add("pressed");
+
+  setTimeout(() => {
+    currentColour.classList.remove("pressed");
+  }, 100);
+}
+
+function playSound(name) {
+  switch (name) {
+    case "green":
+      const green = new Audio("sounds/green.mp3");
+      green.play();
+      break;
+
+    case "blue":
+      const blue = new Audio("sounds/blue.mp3");
+      blue.play();
+      break;
+
+    case "red":
+      const red = new Audio("sounds/red.mp3");
+      red.play();
+      break;
+
+    case "yellow":
+      const yellow = new Audio("sounds/yellow.mp3");
+      yellow.play();
+      break;
+
+    case "wrong":
+      const wrong = new Audio("sounds/wrong.mp3");
+      wrong.play();
+      break;
+
+    default:
+      console.log(colour);
+  }
+}
